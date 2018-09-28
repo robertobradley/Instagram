7d64a419b11d023c8cf8b419c4cd66b5fed73122
user144731 <user144731@74-80-245-6.ic2net.net>
2018-09-27 22:20:51 -0700

    initial push

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by user144731 on 9/27/18.
+//  Copyright © 2018 user144731. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
